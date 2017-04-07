cbe4f57e7f47408e2928b18da651aef4816fcd9b
Logesh Kumar umapathi <infinitylogesh@gmail.com>
2017-04-07 19:48:32 +0530

    Added matplot lib

--- classify.py
+++ classify.py
@@ -4,6 +4,10 @@ from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import learning_curve
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 # Converts the queries to words - Removes stopwords and special characters.
 def queries_to_words(raw_review):
@@ -12,6 +16,35 @@ def queries_to_words(raw_review):
     words = lower_Case.split(" ")
     words = [w for w in words if not w in stopwords.words("english")]
     return " ".join(words)
+
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
+                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
 
 # Train queries are read from the file
 train = pd.read_csv("dataset/TrainingSet.tsv", header=0, delimiter="\t")
@@ -57,16 +90,19 @@ test_data_features = test_data_features.toarray()
 
 
 # Use the random forest to make sentiment label predictions
-result = forest.predict(test_data_features.todense())
+result = forest.predict(test_data_features)
 print zip(vectorizer.get_feature_names(),forest.feature_importances_)
 # Copy the results to a pandas dataframe with an "id" column and
 # a "sentiment" column
 output = pd.DataFrame(data={"id":test["query"], "class":result} )
 
 scores = cross_val_score(forest,test_data_features, test["class"], cv=5)
-print scores
+train_sizes, train_scores, valid_scores =  learning_curve(
+    RandomForestClassifier(n_estimators = 100), test_data_features, test["class"], cv=5, train_sizes=[50, 80, 110])
+    # plot_learning_curve(forest,"First Plot",test_data_features, test["class"], ylim=(0.7, 1.01), train_sizes=[50, 80, 110], cv=5)
+print train_sizes, train_scores, valid_scores
 # Use pandas to write the comma-separated output file
-
+plt.show()
 output.to_csv( "Forest_EventsClassification_Bag_of_Words_model.csv", index=False, quoting=3 )
 
 while(True):
